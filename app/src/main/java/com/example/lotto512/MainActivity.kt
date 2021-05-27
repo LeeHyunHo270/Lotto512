3a49afa8bf900023d505f569ecb16cfdd25cae63
LeeHyunHo270 <lash270@gmail.com>
2021-05-27 13:25:52 +0900

    Random number generation

--- app/src/main/java/com/example/lotto512/MainActivity.kt
+++ app/src/main/java/com/example/lotto512/MainActivity.kt
@@ -3,28 +3,72 @@ package com.example.lotto512
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.EditText
 import androidx.cardview.widget.CardView
+import java.util.*
+import kotlin.collections.ArrayList
+
+fun getRandomLottoNumber(): Int{
+    return Random().nextInt(45)+1
+}
+
+fun getRandomLottoNumbers(): MutableList<Int> {
+    val lottoNumbers = mutableListOf<Int>()
+
+    while (true) {
+        val number = getRandomLottoNumber()
+        var flag_exist = 0;
+
+        if (lottoNumbers.size < 1) {
+            lottoNumbers.add(number)
+            continue
+        } else {
+            for (j in 0 until lottoNumbers.size) {
+                if (number == lottoNumbers[j]) {
+                    flag_exist = 1
+                    break
+                }
+            }
+            if (flag_exist == 0)
+                lottoNumbers.add(number)
+            if (lottoNumbers.size >= 6)
+                break
+        }
+    }
+        return lottoNumbers
+    }
 
 class MainActivity : AppCompatActivity() {
-
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        var intent: Intent
-        var CV2 = findViewById<CardView>(R.id.CV2)
-        val CardViewName = findViewById<CardView>(R.id.CardViewName)
+        var cardConstell = findViewById<CardView>(R.id.cardConstell)
+        val cardName = findViewById<CardView>(R.id.CardViewName)
+        val cardRandom = findViewById<CardView>(R.id.cardRandom)
 
-        CV2.setOnClickListener {
+        cardConstell.setOnClickListener {
             startActivity(Intent(this@MainActivity, ConstellationActivity::class.java))
 
         }
-        CardViewName.setOnClickListener {
+        cardRandom.setOnClickListener{
+            startActivity(Intent(this@MainActivity, ResultActivity::class.java))
+            val intent = Intent(this, ResultActivity::class.java)
+            intent.putIntegerArrayListExtra("result", ArrayList(getRandomLottoNumber()))
+            startActivity(intent)
+        }
+        cardName.setOnClickListener {
             startActivity(Intent(this@MainActivity, NameActivity::class.java))
 
         }
-
+        val numbers = { listOf(1,2,3,4)}
+        /*println(numbers)
+        numbers.forEach{numbers -> println(numbers)}
+        numbers.forEach{numbers -> println(number)}
+        numbers.forEach{println(it)}
+        val mutableNumbers = mutableListOf<Int>(5,6,7,8)*/
     }
+
+    
+
+
 }
